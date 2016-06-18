e59741ef506e8146b431e04ea609511166b85595
olliecad1 <officialgamermorris@gmail.com>
2016-06-18 18:20:39 +0000

    Setting_up_Startcoin

---
+++ README.md
@@ -0,0 +1,7 @@
+Bash script for setting up Startcoin on Raspberry Pi 
+
+1. Downloading the Bash script git clone https://github.com/Olliecad1/Startcoin_setup/blob/master/Startcoin.sh
+
+2. Executing the Bash script sudo chmod +x ./Startcoin.sh
+
+3. Running the Script ./Startcoin.sh
---
+++ Startcoin.sh
@@ -0,0 +1,36 @@
+sed '16d' /etc/dphys-swapfile
+sed 's/16d/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+mkdir ~/startcoin
+cd ~/startcoin
+sudo apt-get install git nano make automake build-essential libssl-dev libboost-all-dev -y
+wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+tar -xzvf db-4.8.30.NC.tar.gz
+cd db-4.8.30.NC/build_unix; ../dist/configure --enable-cxx
+make
+sudo make install
+sudo ln -s /usr/local/BerkeleyDB.4.8/lib/libdb-4.8.so /usr/lib/libdb-4.8.so
+sudo ln -s /usr/local/BerkeleyDB.4.8/lib/libdb_cxx-4.8.so /usr/lib/libdb_cxx.so
+sudo ln -s /usr/local/BerkeleyDB.4.8/lib/libdb_cxx-4.8.so /usr/lib/libdb_cxx-4.8.so
+sudo ln -s /usr/local/BerkeleyDB.4.8/include/db_cxx.h /usr/include/db_cxx.h 
+cd ~/startcoin
+git clone https://github.com/startcoin-project/startcoin.git
+cd ~/startcoin/startcoin/src/
+mkdir obj
+export BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.4.8/include"
+export BDB_LIB_PATH="/usr/local/BerkeleyDB.4.8/lib"
+make -f makefile.unix USE_UPNP=-
+sudo cp startcoind /usr/local/bin
+cd ~/.startcoin-v2
+touch startcoin.conf
+printf 'listen=1\n   server=1\n  daemon=1\n   testnet=0\n   rpcuser=STARTCOIN\n   rpcpassword=STARTCOIN_TEST' >> startcoin.conf
+chmod 700 startcoin.conf
+chmod 700 ~/.startcoin-v2
+cd ~/startcoin
+wget https://www.dropbox.com/s/c45nxw29xk20af2/startcoin-qt.tgz
+tar -xzvf startcoin-qt.tgz
+startcoind stop
+cd ~/startcoin
+sudo cp startcoin-qt /usr/local/bin
+startcoin-qt
